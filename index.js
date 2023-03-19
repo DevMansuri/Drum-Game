084f37f4118f1b9bc793ab954f3954a7a186ab39
“Nadim” <nadimmansu@gmail.com>
2023-03-19 17:30:06 +0530

    Update file : Add diffrent sound on each button

--- index.js
+++ index.js
@@ -5,8 +5,44 @@ for (var  i = 0 ;  i < number ; i++ ) {
 
 }
 function handleClick() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
 
+    var buttonHtml = this.innerHTML;
+
+    switch (buttonHtml) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "a":
+            var tom1 = new Audio("sounds/tom-2.mp3");
+            tom1.play();
+        break;
+        case "s":
+            var tom1 = new Audio("sounds/tom-3.mp3");
+            tom1.play();
+        break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-4.mp3");
+            tom1.play();
+        break;
+        case "j":
+            var tom1 = new Audio("sounds/snare.mp3");
+            tom1.play();
+        break;
+        case "k":
+            var tom1 = new Audio("sounds/crash.mp3");
+            tom1.play();
+        break;
+        case "l":
+            var tom1 = new Audio("sounds/kick-bass.mp3");
+            tom1.play();
+        break;
+    
+    
+        default:
+            console.log(buttonHtml);
+        break;
+    }
+    
 // alert("click");
 }
