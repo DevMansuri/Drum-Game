76ca671e569218d4ae7cf8a87dc255f554f536e8
“Nadim” <nadimmansu@gmail.com>
2023-03-19 17:53:46 +0530

    Update Js file : Add key press for make sound

--- index.js
+++ index.js
@@ -7,42 +7,49 @@ for (var  i = 0 ;  i < number ; i++ ) {
 function handleClick() {
 
     var buttonHtml = this.innerHTML;
+    makeSound(buttonHtml);
+}
 
-    switch (buttonHtml) {
-        case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-        break;
-        case "a":
-            var tom1 = new Audio("sounds/tom-2.mp3");
-            tom1.play();
-        break;
-        case "s":
-            var tom1 = new Audio("sounds/tom-3.mp3");
-            tom1.play();
-        break;
-        case "d":
-            var tom1 = new Audio("sounds/tom-4.mp3");
-            tom1.play();
-        break;
-        case "j":
-            var tom1 = new Audio("sounds/snare.mp3");
-            tom1.play();
-        break;
-        case "k":
-            var tom1 = new Audio("sounds/crash.mp3");
-            tom1.play();
-        break;
-        case "l":
-            var tom1 = new Audio("sounds/kick-bass.mp3");
-            tom1.play();
-        break;
-    
-    
-        default:
-            console.log(buttonHtml);
-        break;
+document.addEventListener("keydown", function(event) {
+makeSound(event.key);
+})
+
+    function makeSound(key) {
+        switch (key) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+            break;
+            case "a":
+                var tom1 = new Audio("sounds/tom-2.mp3");
+                tom1.play();
+            break;
+            case "s":
+                var tom1 = new Audio("sounds/tom-3.mp3");
+                tom1.play();
+            break;
+            case "d":
+                var tom1 = new Audio("sounds/tom-4.mp3");
+                tom1.play();
+            break;
+            case "j":
+                var tom1 = new Audio("sounds/snare.mp3");
+                tom1.play();
+            break;
+            case "k":
+                var tom1 = new Audio("sounds/crash.mp3");
+                tom1.play();
+            break;
+            case "l":
+                var tom1 = new Audio("sounds/kick-bass.mp3");
+                tom1.play();
+            break;
+        
+        
+            default:
+                console.log(buttonHtml);
+            break;
+
     }
     
-// alert("click");
 }
